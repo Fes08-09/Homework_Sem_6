d8295d3a72b08b07797c1dd8c95cbf4ef890ab62
Dmitriy <Fes08-09@yandex.ru>
2022-07-28 14:04:57 +0300

    Homework_sem_6_done

---
+++ Task_41/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Введите числа через пробел: ");
+int[] array = GetArrayFromString(ReadLine());
+WriteLine($"{string.Join(", ", array)} -> {CountPositiveNumbres(array)}");
+
+int[] GetArrayFromString(string arrayStr)
+{
+    string[] arS=arrayStr.Split(new char[]{' ',',','*'},StringSplitOptions.RemoveEmptyEntries); // вот этот пример!!!
+    int[] result=new int[arS.Length];
+    for(int i=0; i<arS.Length;i++)
+        {
+        result[i]=int.Parse(arS[i]);
+        }
+    return result;
+}
+
+int CountPositiveNumbres(int [] arr)
+{
+    int count = 0;
+    for(int i =0; i<arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count+=1;
+        }
+    }
+    return count;
+}
---
+++ Task_43/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+
+using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Введите значение b1: ");
+double b1 = double.Parse(ReadLine());
+WriteLine("Введите значение k1: ");
+double k1 = double.Parse(ReadLine());
+WriteLine("Введите значение b2: ");
+double b2 = double.Parse(ReadLine());
+WriteLine("Введите значение k2: ");
+double k2 = double.Parse(ReadLine());
+WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({CrossingTwoLines(b1, k1, b2, k2)})");
+
+string CrossingTwoLines(double number1, double number11, double number2, double number22)
+{
+    string crossing = "";
+    double x = (number2 - number1)/ (number11 - number22);
+    double y = number11 * x + number1;
+    crossing = $"{x}; {y}";
+    return crossing;
+}
